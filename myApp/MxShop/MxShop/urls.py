4e05f851bec951fcc31160edd4e9637b2a0c4072
wujun0919 <wujun0919@qq.com>
2020-05-08 00:09:42 +0800

    05071

--- myApp/MxShop/MxShop/urls.py
+++ myApp/MxShop/MxShop/urls.py
@@ -19,8 +19,9 @@ from django.urls import path
 from django.urls import re_path
 from django.views.static import serve
 from .settings import MEDIA_ROOT
-
+from goods.view_base import GoodsListView
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
+    re_path(r'goods/$', GoodsListView.as_view(), name='goods-list')
 ]
